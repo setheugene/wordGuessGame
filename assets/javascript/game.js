9fa579d8786d7062d391ea6a7df1a7f756abf4bf
setheugene <seth.eugene@icloud.com>
2018-12-03 20:37:39 -0600

    still lost

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,52 +3,66 @@ window.onload = function () {
 //computer picks random word
     var computerWords = ["cat", "dog", "dolphin", "bear", "shark"];
     var word = computerWords[Math.floor(Math.random() * computerWords.length)];
+console.log(word);
 
-//loop for filling in answer
-var blank = [];
-for (var i = 0; i < word.length; i++) {
-blank[i] = "_";
+
+var blank = "";
+
+wordArray = word.length
+//wordLetters =
+
+//wordArray is equal to word letters
+var wordArray = function () {for (var i = 0; i < word.length; i++) {
+    blank[i] = "_";
+
 }
+console.log(blank);
+
 
 var remainingLetters = word.length;
 
-// console.log (remainingLetters);
+console.log (remainingLetters);
 
 //player needs to see score
 var wins = 0;
 //player needs to see empty spaces filling in and guesses left and guessed
-var blank = word;
+
 var guessesRemaining = 10;
-var userInput = [];
+
+
+
 
 //record guess
 document.onkeyup = function(event) {
+
     var userInput = event.key;
 
-
 //if guess is correct update blank var
-if (userInput === word) {
+if (userInput === wordArray) {
     //word fills in blank var
 }
 
-else if (userInput !== word) {
+else if (userInput !== wordArray) {
     //take away guesses remaining
 guessesRemaining --;
     //update guessed letters
 }
 }
 
-//win alert
 console.log (userInput);
+
+//win alert
+
 
 
 //change text accordingly
 var winsText = document.getElementById("wins-text");
 var blankText = document.getElementById("blank-text");
-var guessesRemainingText = document.getElementById("number-text")
-var userInput = document.getElementById("lettersGuessed-text")
+var guessesRemainingText = document.getElementById("number-text");
+var userInput = document.getElementById("lettersGuessed-text");
 
 winsText.textContent = "wins: " + wins;
-blankText.textContent = blank;
+blankText.textContent = wordArray;
 guessesRemainingText.textContent = guessesRemaining;
-userInput.textcontent = userInput;}
+userInput.textcontent = userInput;
+}}
