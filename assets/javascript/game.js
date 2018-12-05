4cc673e0a7cdb2c2a3faf65114341e74c43b7014
setheugene <seth.eugene@icloud.com>
2018-12-05 15:46:15 -0600

    doneish

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,68 +1,92 @@
-window.onload = function () {
-
-//computer picks random word
-    var computerWords = ["cat", "dog", "dolphin", "bear", "shark"];
-    var word = computerWords[Math.floor(Math.random() * computerWords.length)];
-console.log(word);
-
-
-var blank = "";
-
-wordArray = word.length
-//wordLetters =
-
-//wordArray is equal to word letters
-var wordArray = function () {for (var i = 0; i < word.length; i++) {
-    blank[i] = "_";
-
-}
-console.log(blank);
-
-
-var remainingLetters = word.length;
-
-console.log (remainingLetters);
-
-//player needs to see score
-var wins = 0;
-//player needs to see empty spaces filling in and guesses left and guessed
-
+var computerLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var guessesRemaining = 10;
+var guessedLetters = [];
+var currentLetter = [];
+var wins = 0;
+var hasWon= false;
+var letter = {func: computerLetters[Math.floor(Math.random() * computerLetters.length)]}
+console.log(letter);
 
+window.onload = function() {
 
+  document.getElementById("currentLetter-text").style.cssText= "display: none";
 
+  document.getElementById("wins-text").innerText = wins;
 
-//record guess
-document.onkeyup = function(event) {
+  //computer picks random word
 
+ 
+
+  //record guess
+  document.onkeyup = function(event) {
     var userInput = event.key;
 
-//if guess is correct update blank var
-if (userInput === wordArray) {
-    //word fills in blank var
-}
-
-else if (userInput !== wordArray) {
-    //take away guesses remaining
-guessesRemaining --;
-    //update guessed letters
-}
-}
-
-console.log (userInput);
-
-//win alert
 
 
+     if (userInput === letter.func && event.keyCode !== 32) {
+      var blankText = document.getElementById("blank-text").innerText = letter.func;
+      document.getElementById("currentLetter-text").style.cssText= "display: block";
+      document.getElementById("blank-text").style.backgroundColor= "green";
+      hasWon = true;
+      //add one
+      wins++;
+      
+    }
 
-//change text accordingly
-var winsText = document.getElementById("wins-text");
-var blankText = document.getElementById("blank-text");
-var guessesRemainingText = document.getElementById("number-text");
-var userInput = document.getElementById("lettersGuessed-text");
+
+    else if (event.keyCode === 32) {
+      document.getElementById("blank-text").style.cssText= "display: reset";
+      document.getElementById("currentLetter-text").style.cssText= "display: none";
+      document.getElementById("blank-text").style.backgroundColor= "reset";
+      var blankText = document.getElementById("blank-text").innerText = "";
+      var guessedLetters = document.getElementById("lettersGuessed-text").innerText = "";
+      hasWon = false;
+
+    }
+
+
+
+    else if (userInput !== letter.func) {
+      document.getElementById("blank-text").style.cssText= "display: reset";
+      document.getElementById("currentLetter-text").style.cssText= "display: none";
+      document.getElementById("blank-text").style.backgroundColor= "reset";
+      var blankText = document.getElementById("blank-text").innerText = "";
+      //remove one
+      guessesRemaining--;
+      //append?
+      // userInput.append(guessedLetters);
+      // document.getElementById("lettersGuessed-text").append(guessedLetters);
+    }
+
+    
+  
+    console.log(userInput);
+    console.log(hasWon);
+    console.log(letter);
+  }
+
+
+
+
+  //win alert
+
+  //change text accordingly
+  
+  var winsText = document.getElementById("wins-text");
+  var blankText = document.getElementById("blank-text");
+  var guessesRemainingText = document.getElementById("number-text");
+  var userInput = document.getElementById("lettersGuessed-text");
 
 winsText.textContent = "wins: " + wins;
-blankText.textContent = wordArray;
+blankText.textContent = currentLetter;
 guessesRemainingText.textContent = guessesRemaining;
 userInput.textcontent = userInput;
-}}
+
+;
+}
+
+function done() {
+  if (hasWon === true) {
+    wins++;
+  }
+}
