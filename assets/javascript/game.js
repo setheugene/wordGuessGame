c1aee89adf13eef6ffeafdaf6ce506b5fa90dc51
setheugene <seth.eugene@icloud.com>
2018-12-05 16:11:53 -0600

    new letter works

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -13,11 +13,6 @@ window.onload = function() {
 
   document.getElementById("wins-text").innerText = wins;
 
-  //computer picks random word
-
- 
-
-  //record guess
   document.onkeyup = function(event) {
     var userInput = event.key;
 
@@ -41,21 +36,23 @@ window.onload = function() {
       var blankText = document.getElementById("blank-text").innerText = "";
       var guessedLetters = document.getElementById("lettersGuessed-text").innerText = "";
       hasWon = false;
-
+      //pick a new letter
+      letter = {func: computerLetters[Math.floor(Math.random() * computerLetters.length)]}
+console.log(letter);
     }
 
 
 
-    else if (userInput !== letter.func) {
+    else if (userInput !== letter.func && event.keyCode !== 32) {
       document.getElementById("blank-text").style.cssText= "display: reset";
       document.getElementById("currentLetter-text").style.cssText= "display: none";
-      document.getElementById("blank-text").style.backgroundColor= "reset";
       var blankText = document.getElementById("blank-text").innerText = "";
       //remove one
       guessesRemaining--;
-      //append?
-      // userInput.append(guessedLetters);
-      // document.getElementById("lettersGuessed-text").append(guessedLetters);
+      //append guess
+      /*userInput.append(guessedLetters);
+      document.getElementById("lettersGuessed-text").append(guessedLetters);
+      */
     }
 
     
@@ -65,12 +62,6 @@ window.onload = function() {
     console.log(letter);
   }
 
-
-
-
-  //win alert
-
-  //change text accordingly
   
   var winsText = document.getElementById("wins-text");
   var blankText = document.getElementById("blank-text");
@@ -83,10 +74,4 @@ guessesRemainingText.textContent = guessesRemaining;
 userInput.textcontent = userInput;
 
 ;
-}
-
-function done() {
-  if (hasWon === true) {
-    wins++;
-  }
 }
